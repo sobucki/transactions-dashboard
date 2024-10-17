2576ff5da2d8954471297ca45445f0a66b5bdff2
Rafael Soares Sobucki <rafaelsobucki.smartsoftware@bexsbanco.com.br>
2024-10-17 16:20:13 -0300

    Configuration styled-components

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { CustomHeader } from "./styles";
+
+function Header() {
+  return (
+    <div>
+      <CustomHeader>Teste</CustomHeader>
+    </div>
+  );
+}
+
+export default Header;
