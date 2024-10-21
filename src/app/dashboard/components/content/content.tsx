5202ca743466d4e63e57f763de7f4bfc86bd02d1
Rafael Soares Sobucki <rafaelsobucki.smartsoftware@bexsbanco.com.br>
2024-10-20 22:46:59 -0300

    Save filter on cookies

--- src/app/dashboard/components/content/content.tsx
+++ src/app/dashboard/components/content/content.tsx
@@ -2,20 +2,34 @@
 
 import { FilterOptions } from "@/app/api/transactions/types";
 import { Box, Toolbar, Grid } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import IndustryBalanceBarChart from "../charts/balance/industry-balance-bar-chart";
 import IndustryAmountBarChart from "../charts/sum/industry-amount-bar-chart";
 import StateBalanceBarChart from "../charts/balance/state-balance-bar-chart";
 import IndustryMovementPieChart from "../charts/total/industry-movement-pie-chart";
 import Summary from "../summary";
 import Filter from "../filter";
+import Cookies from "js-cookie";
 
 function Content() {
-  const [filterOptions, setFilterOptions] = useState<FilterOptions>({});
+  const [filterOptions, setFilterOptions] = useState<FilterOptions | null>(
+    null
+  );
+
+  useEffect(() => {
+    const savedFilter = Cookies.get("filter");
+    const initialFilter = savedFilter ? JSON.parse(savedFilter) : {};
+    setFilterOptions(initialFilter);
+  }, []);
 
   const handleFilterChange = (filters: FilterOptions) => {
+    Cookies.set("filter", JSON.stringify(filters));
     setFilterOptions(filters);
   };
+
+  if (filterOptions === null) {
+    return null;
+  }
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
