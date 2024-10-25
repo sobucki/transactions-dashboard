58d9e2634ed415a6745c955afdefd33d4355c88b
Rafael Soares Sobucki <rafaelsobucki.smartsoftware@bexsbanco.com.br>
2024-10-25 14:38:44 -0300

    Fix suspense and change api to force-dynamic

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -5,6 +5,8 @@ import { Transaction } from "./types";
 import { filterTransactions } from "./util/service-util";
 import { buildFilterFromRequest } from "./util/filter";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   try {
     const filePath = path.join(process.cwd(), "data/transactions.json");
