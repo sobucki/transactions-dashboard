34bf439ec761fc346cab0d233b4700a89faa1b1a
Rafael Sobucki <sobucki@Mac-mini-de-Rafael.local>
2024-10-27 17:48:35 -0300

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Theme from "./themes";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Dashboard Transactions",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <Theme>{children}</Theme>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
